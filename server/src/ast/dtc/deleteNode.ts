18d9ec7693b3a8231e3812c52b22f4eaa8648334
Kyle Bonnici <kylebonnici@MacBookPro.lan>
2024-12-01 05:14:02 +0100

    Linting

--- server/src/ast/dtc/deleteNode.ts
+++ server/src/ast/dtc/deleteNode.ts
@@ -1,26 +1,24 @@
-import { BuildSemanticTokensPush } from '../../types';
-import { ASTBase } from '../base';
-import { Keyword } from '../keyword';
-import { DocumentSymbol, SymbolKind } from 'vscode-languageserver';
-import { toRange } from '../../helpers';
-import { NodeName } from './node';
-import { LabelRef } from './labelRef';
-import { DeleteBase } from './delete';
+import { Keyword } from "../keyword";
+import { NodeName } from "./node";
+import { LabelRef } from "./labelRef";
+import { DeleteBase } from "./delete";
+import { NodePathRef } from "./values/nodePath";
 
 export class DeleteNode extends DeleteBase {
-	private _nodeNameOrRef: NodeName | LabelRef | null = null;
+  private _nodeNameOrRef: NodeName | LabelRef | NodePathRef | null = null;
 
-	constructor(keyword: Keyword) {
-		super('Delete Node', keyword);
-	}
+  constructor(keyword: Keyword) {
+    super("Delete Node", keyword);
+  }
 
-	set nodeNameOrRef(nodeNameOrRef: NodeName | LabelRef | null) {
-		if (this._nodeNameOrRef) throw new Error('Only on property name is allowed');
-		this._nodeNameOrRef = nodeNameOrRef;
-		this.addChild(nodeNameOrRef);
-	}
+  set nodeNameOrRef(nodeNameOrRef: NodeName | LabelRef | NodePathRef | null) {
+    if (this._nodeNameOrRef)
+      throw new Error("Only on property name is allowed");
+    this._nodeNameOrRef = nodeNameOrRef;
+    this.addChild(nodeNameOrRef);
+  }
 
-	get nodeNameOrRef() {
-		return this._nodeNameOrRef;
-	}
+  get nodeNameOrRef() {
+    return this._nodeNameOrRef;
+  }
 }
--- server/src/ast/dtc/values/nodePath.ts
+++ server/src/ast/dtc/values/nodePath.ts
@@ -1,28 +1,32 @@
-import { ASTBase } from '../../base';
-import { NodeName } from '../node';
+import { ASTBase } from "../../base";
+import { NodeName } from "../node";
 
 export class NodePath extends ASTBase {
-	private _pathParts: (NodeName | null)[] = [];
+  private _pathParts: (NodeName | null)[] = [];
 
-	constructor() {
-		super();
-	}
+  constructor() {
+    super();
+  }
 
-	addPath(part: NodeName | null) {
-		this._pathParts.push(part);
-		this.addChild(part);
-	}
+  addPath(part: NodeName | null) {
+    this._pathParts.push(part);
+    this.addChild(part);
+  }
 
-	get pathParts() {
-		return [...this._pathParts];
-	}
+  get pathParts() {
+    return [...this._pathParts];
+  }
+
+  toString() {
+    return this._pathParts.map((p) => p?.toString() ?? "<NULL>").join("/");
+  }
 }
 
 export class NodePathRef extends ASTBase {
-	constructor(public readonly path: NodePath | null) {
-		super();
-		this.semanticTokenType = 'variable';
-		this.semanticTokenModifiers = 'declaration';
-		this.addChild(path);
-	}
+  constructor(public readonly path: NodePath | null) {
+    super();
+    this.semanticTokenType = "variable";
+    this.semanticTokenModifiers = "declaration";
+    this.addChild(path);
+  }
 }
