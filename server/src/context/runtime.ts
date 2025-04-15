3d00fa1cc8a2f65a3f1483d73a6aa0791093f73f
Kyle Micallef Bonnici <kyle.bonnici@nordicsemi.no>
2025-04-15 16:54:26 +0200

    Cache search result

--- server/src/context/runtime.ts
+++ server/src/context/runtime.ts
@@ -65,8 +65,13 @@ export class Runtime implements Searchable {
       : astNode.children.flatMap((c) => Runtime.getFileTopMostAst(c, file));
   };
 
+  private fileTopMostAstCache = new Map<string, ASTBase[]>();
+
   fileTopMostAst(file: string) {
-    return [
+    const cache = this.fileTopMostAstCache.get(file);
+    if (cache) return cache;
+    // TODO consider a different way to optain this as this is costly
+    const result = [
       ...this.roots,
       ...this.references,
       ...this.unlinkedDeletes,
@@ -77,6 +82,9 @@ export class Runtime implements Searchable {
         (op) => op.cPreprocessorParser.allAstItems
       ),
     ].flatMap((c) => Runtime.getFileTopMostAst(c, file));
+
+    this.fileTopMostAstCache.set(file, result);
+    return result;
   }
 
   getDeepestAstNode(
