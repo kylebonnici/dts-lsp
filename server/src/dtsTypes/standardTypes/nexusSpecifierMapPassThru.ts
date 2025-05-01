009689b08792374f94a70ae0d0cbcac6b00e7559
Kyle Micallef Bonnici <kylebonnici@hotmail.com>
2025-05-01 13:21:24 +0200

    Validate nexus-map-pass-thru cell count

--- server/src/dtsTypes/standardTypes/nexusSpecifierMapPassThru.ts
+++ server/src/dtsTypes/standardTypes/nexusSpecifierMapPassThru.ts
@@ -14,14 +14,71 @@
  * limitations under the License.
  */
 
+import { FileDiagnostic, StandardTypeIssue } from "../../types";
 import { BindingPropertyType } from "../../types/index";
 import { PropertyNodeType } from "../types";
-import { generateOrTypeObj } from "./helpers";
+import {
+  flatNumberValues,
+  generateOrTypeObj,
+  getU32ValueFromProperty,
+} from "./helpers";
+import { genStandardTypeDiagnostic } from "../../helpers";
+import { DiagnosticSeverity } from "vscode-languageserver-types";
 
 export default () => {
-  const prop = new PropertyNodeType(
+  const prop = new PropertyNodeType<number>(
     /^(?!interrupt-).*?-map-pass-thru$/,
-    generateOrTypeObj(BindingPropertyType.PROP_ENCODED_ARRAY)
+    generateOrTypeObj(BindingPropertyType.PROP_ENCODED_ARRAY),
+    "optional",
+    undefined,
+    undefined,
+    (property, macros) => {
+      const issues: FileDiagnostic[] = [];
+      const node = property.parent;
+
+      const values = flatNumberValues(property.ast.values);
+      if (!values?.length) {
+        return [];
+      }
+
+      const specifier = property.name.split("-map", 1)[0];
+      const childSpecifierCells = node.getProperty(`#${specifier}-cells`);
+
+      if (!childSpecifierCells) {
+        return issues;
+      }
+
+      const childInterruptSpecifierValue = getU32ValueFromProperty(
+        childSpecifierCells,
+        0,
+        0,
+        macros
+      );
+
+      if (childInterruptSpecifierValue == null) {
+        return issues;
+      }
+
+      prop.typeExample = `<${[
+        ...Array.from({ length: childInterruptSpecifierValue }, () => "mask"),
+      ].join(" ")}> `;
+
+      if (values.length !== childInterruptSpecifierValue) {
+        issues.push(
+          genStandardTypeDiagnostic(
+            StandardTypeIssue.CELL_MISS_MATCH,
+            property.ast.values ?? property.ast,
+            DiagnosticSeverity.Error,
+            [],
+            [],
+            [property.name, prop.typeExample]
+          )
+        );
+        return issues;
+      }
+
+      return issues;
+    }
   );
   prop.description = [
     "A `<specifier>-map-pass-thru` property may be specified for a nexus node. This property specifies a mask that is applied to the child unit specifier being looked up in the table specified in the `<specifier>-map` property. Any matching bits in the child unit specifier are copied over to the parent specifier. If this property is not specified, the mask isassumedtobe amaskwithno bits set.",
