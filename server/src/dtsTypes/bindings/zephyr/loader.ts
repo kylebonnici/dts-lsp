d6c803fa526c88ec2720eb73d562ad9376a8e966
Kyle Micallef Bonnici <kylebonnici@hotmail.com>
2025-05-04 21:33:05 +0200

    Add licence

--- server/src/dtsTypes/bindings/zephyr/loader.ts
+++ server/src/dtsTypes/bindings/zephyr/loader.ts
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 Kyle Micallef Bonnici
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { getStandardType } from "../../../dtsTypes/standardTypes";
 import { NodeType, PropertyNodeType } from "../../../dtsTypes/types";
 import { Node } from "../../../context/node";
@@ -17,12 +33,10 @@ import {
   MacroRegistryItem,
   StandardTypeIssue,
 } from "../../../types";
-import { createTokenIndex, genStandardTypeDiagnostic } from "../../../helpers";
+import { genStandardTypeDiagnostic } from "../../../helpers";
 import { DiagnosticSeverity, DiagnosticTag } from "vscode-languageserver";
 import { Property } from "../../../context/property";
 import { BindingPropertyType } from "../../../types/index";
-import { Expression } from "src/ast/cPreprocessors/expression";
-import { ASTBase } from "src/ast/base";
 
 type ZephyrPropertyType =
   | "string"
