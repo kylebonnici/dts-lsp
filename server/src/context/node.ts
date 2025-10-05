8c2cb5605d97573942631cbccfc5a9663ef7c544
Kyle Micallef Bonnici <kylebonnici@hotmail.com>
2025-10-05 08:24:08 +0200

    Add property parent node path to object and type

--- server/src/context/node.ts
+++ server/src/context/node.ts
@@ -1321,7 +1321,7 @@ ${'\t'.repeat(level - 1)}}; ${isOmmited ? ' */' : ''}`;
 			nodeType:
 				nodeType instanceof NodeType
 					? {
-							...this.nodeType,
+							...nodeType,
 							extends: Array.from(this.nodeType?.extends ?? []),
 							properties: nodeType.properties.map((p) => ({
 								name:
@@ -1345,6 +1345,7 @@ ${'\t'.repeat(level - 1)}}; ${isOmmited ? ' */' : ''}`;
 			nodes: nodeAsts.map((d) => d.serialize(macros)),
 			properties: this.properties.map((p) => ({
 				...p.ast.serialize(macros),
+				nodePath: this.pathString,
 				nexusMapEnty: p.nexusMapsTo.map((nexus) => {
 					return {
 						mappingValuesAst: nexus.mappingValuesAst.map((v) =>
