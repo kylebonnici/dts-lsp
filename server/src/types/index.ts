04e2af3d51414214e2e4c2ce7a1adc6200f112fd
Kyle Micallef Bonnici <kylebonnici@hotmail.com>
2025-10-05 08:22:27 +0200

    Fix: property name type to never be null

--- server/src/types/index.ts
+++ server/src/types/index.ts
@@ -190,8 +190,7 @@ export type SerializableNexusMapEnty = {
 
 export interface SerializableProperty extends SerializableASTBase {
 	readonly nexusMapEnty: SerializableNexusMapEnty[];
-	readonly name: SerializablePropertyName | null;
-	readonly values: SerializablePropertyValue[] | null;
+	readonly name: SerializablePropertyName;
 }
 
 export type SerializableDtcProperty = Omit<
