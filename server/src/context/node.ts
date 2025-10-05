c6e4ad20976c4e64b2dd716ec10247ee3a41a87c
Kyle Micallef Bonnici <kylebonnici@hotmail.com>
2025-10-05 08:20:13 +0200

    Providethe generic binding type for a node with zephyr binding

--- server/src/context/node.ts
+++ server/src/context/node.ts
@@ -1348,6 +1348,7 @@ ${'\t'.repeat(level - 1)}};`;
 								description: p.description?.join('\n'),
 								required: p.required(this) === 'required',
 							})),
+							zephyrBinding: nodeType.zephyrBinding,
 						}
 					: undefined,
 			issues: nodeAsts.flatMap((n) => n.serializeIssues),
