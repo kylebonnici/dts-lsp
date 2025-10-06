5dfb354a0669f0561ed121542e7d502aa179d875
Kyle Micallef Bonnici <kylebonnici@hotmail.com>
2025-10-06 17:21:43 +0200

    Fix: property name in child binding

--- server/src/dtsTypes/bindings/zephyr/loader.ts
+++ server/src/dtsTypes/bindings/zephyr/loader.ts
@@ -131,6 +131,9 @@ const resolveBinding = (
 	bindings: ZephyrBindingYml[],
 	binding: ZephyrBindingYml,
 ): ZephyrBindingYml | undefined => {
+	Object.entries(binding.properties ?? {}).forEach(([name, prop]) => {
+		prop.name = name;
+	});
 	binding.extends ??= [];
 	binding = binding.include.reduce((p, c) => {
 		const toMergeIn = bindings.find((b) => basename(b.filePath) === c.name);
@@ -459,11 +462,6 @@ export class ZephyrBindingsLoader {
 									rawInclude: [...simplifiyedInclude],
 									filePath: bindingFile,
 								} as ZephyrBindingYml;
-								Object.entries(obj.properties ?? {}).forEach(
-									([name, prop]) => {
-										prop.name = name;
-									},
-								);
 								this.zephyrBindingCache.set(bindingFile, obj);
 								return obj;
 							} catch (e) {
