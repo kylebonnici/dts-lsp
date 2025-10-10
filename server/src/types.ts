3937ef93901743874a876fcb1fd2b305e1f37dad
Kyle Micallef Bonnici <kylebonnici@hotmail.com>
2025-10-10 10:04:10 +0200

    Rename type

--- server/src/types.ts
+++ server/src/types.ts
@@ -385,7 +385,7 @@ export type RangeMapping = {
 	rangeTokens: TokenIndexes;
 };
 
-export interface NexusMapEnty {
+export interface NexusMapEntry {
 	mappingValues: (LabelRef | NodePathRef | NumberValue | Expression)[];
 	node: Node;
 	parentValues: (LabelRef | NodePathRef | NumberValue | Expression)[];
