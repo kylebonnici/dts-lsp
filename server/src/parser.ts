a56d21118b20a2a6a536ed3b7d65bcefbb49b28e
Kyle Bonnici <kylebonnici@hotmail.com>
2025-10-28 21:26:16 +0100

    Format macros

--- server/src/parser.ts
+++ server/src/parser.ts
@@ -78,6 +78,7 @@ export class Parser extends BaseParser {
 	public tokens: Token[] = [];
 	cPreprocessorParser: CPreprocessorParser;
 
+	injectedMacros: (CMacroCall | CIdentifier)[] = [];
 	others: ASTBase[] = [];
 	rootDocument = new DtcBaseNode();
 	unhandledStatements = new DtcRootNode();
@@ -115,6 +116,7 @@ export class Parser extends BaseParser {
 	protected reset() {
 		super.reset();
 		this.others = [];
+		this.injectedMacros = [];
 		this.rootDocument = new DtcBaseNode();
 		this._issues = [];
 		this.unhandledStatements = new DtcRootNode();
@@ -235,6 +237,7 @@ export class Parser extends BaseParser {
 			this.processCIdentifier(macros, true, true);
 		let evalResult = result?.resolve(macros);
 		if (result && typeof evalResult === 'string') {
+			this.injectedMacros.push(result);
 			evalResult = sanitizeCExpression(evalResult);
 			const uri = `${result.uri}${VIRTUAL_DOC}${result.firstToken.pos.line}:${result.firstToken.pos.col}-${result.lastToken.pos.line}:${result.firstToken.pos.col}`;
 
