e1060f9398711bf111a1103388f1cb78c732363b
Kyle Bonnici <kylebonnici@hotmail.com>
2025-11-07 20:51:30 +0100

    Add debug remote

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -102,6 +102,7 @@ export async function activate(context: vscode.ExtensionContext) {
 		debug: {
 			module: serverModule,
 			transport: TransportKind.ipc,
+			options: { execArgv: ['--nolazy', '--inspect=6009'] },
 		},
 	};
 
