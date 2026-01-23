60f58bd4f6e07c83267e5ab3893ea68af0e55ff4
Kyle Bonnici <kylebonnici@hotmail.com>
2026-01-23 17:23:02 +0100

    Add Zephyt Prop go to type

--- server/src/typeDefinition.ts
+++ server/src/typeDefinition.ts
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+import { readFileSync } from 'fs';
+import { basename } from 'path';
 import {
 	Location,
 	Position,
@@ -28,6 +30,10 @@ import { Label } from './ast/dtc/label';
 import { LabelRef } from './ast/dtc/labelRef';
 import { nodeFinder, pathToFileURL } from './helpers';
 import { isDeleteChild } from './ast/helpers';
+import { PropertyName } from './ast/dtc/property';
+import { Property } from './context/property';
+import { NodeType } from './dtsTypes/types';
+import { ZephyrBindingYml } from './types/index';
 
 function getNodeTypeDefinition(
 	result: SearchableResult | undefined,
@@ -72,11 +78,94 @@ function getNodeTypeDefinition(
 	return [];
 }
 
+function getPropertyTypeDefinition(
+	result: SearchableResult | undefined,
+): Location[] {
+	if (
+		!result ||
+		!(result.item instanceof Property) ||
+		!(result.ast instanceof PropertyName) ||
+		result.runtime.context.bindingLoader?.type !== 'Zephyr'
+	) {
+		return [];
+	}
+
+	const node = result.item.parent;
+
+	const binding =
+		node.nodeType instanceof NodeType
+			? node.nodeType.zephyrBinding
+			: undefined;
+
+	if (!binding) {
+		return [];
+	}
+
+	const rootRange = findPropInFile(result.ast.name, binding);
+	if (rootRange) {
+		return [Location.create(pathToFileURL(binding.filePath), rootRange)];
+	}
+
+	const extendsBindings = (
+		result.runtime.context.bindingLoader?.getZephyrContextBinding?.() ?? []
+	).filter((b) =>
+		binding.extends?.some((bb) => basename(b.filePath) === `${bb}.yaml`),
+	);
+
+	for (const extBinding of extendsBindings) {
+		const r = findPropInFile(result.ast.name, extBinding);
+		if (r) {
+			return [Location.create(pathToFileURL(extBinding.filePath), r)];
+		}
+	}
+
+	return [
+		Location.create(
+			pathToFileURL(binding.filePath),
+			Range.create(Position.create(0, 0), Position.create(0, 0)),
+		),
+	];
+}
+
 export async function typeDefinition(
 	location: TypeDefinitionParams,
 	context?: ContextAware,
 ): Promise<Location[]> {
 	return nodeFinder(location, context, (locationMeta) => [
 		...getNodeTypeDefinition(locationMeta),
+		...getPropertyTypeDefinition(locationMeta),
 	]);
+}
+
+function findPropInFile(
+	propertyName: string,
+	binding: ZephyrBindingYml,
+): Range | undefined {
+	const typeFile = binding.filePath;
+	const text = readFileSync(typeFile, 'utf8');
+	return getLocation(
+		text,
+		new RegExp(`(${propertyName}|"${propertyName}")\\s*:`),
+	);
+}
+
+function getLocation(text: string, regex: RegExp) {
+	const match = text.match(regex);
+	if (!match) return;
+
+	const index = match.index;
+	if (index === undefined) return;
+	const before = text.slice(0, index);
+
+	const line = before.split('\n').length - 1;
+
+	const lastNewline = before.lastIndexOf('\n');
+	const startCol = lastNewline === -1 ? index + 1 : index - lastNewline;
+
+	const endCol = startCol + match[0].length;
+
+	return Range.create(
+		Position.create(line, startCol),
+		Position.create(line, endCol),
+	);
 }
